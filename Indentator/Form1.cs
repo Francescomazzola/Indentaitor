739b3561c844297f9aea19f20f655fe362225c62
lorethefixer <lore007@hotmail.it>
2018-11-13 10:00:00 +0100

    second commit, second gui prealpha

--- Indentator/Form1.cs
+++ Indentator/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +13,37 @@ namespace Indentator
 {
     public partial class Form1 : Form
     {
+        public string percorso;
+        private object openFileDialog1;
+
         public Form1()
         {
             InitializeComponent();
         }
-        
-    }
+
+        private void selectfileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "cs",
+                Filter = "cs files (*.cs)|*.cs",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                percorso=openFileDialog1.FileName;
+            }
+        }
+     }
 }
